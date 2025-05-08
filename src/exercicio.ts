ee61bb7b15315b66958a7bf2114669502779e2bf
arnor <arnorsantosnunes01@gmail.com>
2025-05-08 20:19:11 -0300

    iniciando exercicio_ts

---
+++ src/exercicio.ts
@@ -0,0 +1,9 @@
+function multiplicacao (a: number, b: number): number {
+    return a * b;
+};
+
+function saudacao(nome: string): string {
+    return 'Olá ' + nome;
+}
+console.log(multiplicacao(5, 10));
+console.log(saudacao("Arnor"));
